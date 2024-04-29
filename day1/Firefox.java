3906501e84ec536f92dd2e88f3e769384082a49d
UmaGithub01 <168459174+UmaGithub01@users.noreply.github.com>
2024-04-29 10:49:24 -0700

    Add files via upload
    Day1

---
+++ day1/Firefox.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Firefox {
+public static void main(String[] args) {
+	//Variable Declarations
+	float 	browserVersion 	= 100.2f;
+	String 	browserName 	= "firefox";
+	boolean isVisible 		= true;
+	int 	releaseYear 	= 1998;
+	char 	browserLogo 	= 'f';
+	//To print the details
+	System.out.println("Browser Version :"+ browserVersion);
+	System.out.println("Browser Name :"+ browserName);
+	System.out.println("Is Visible :"+ isVisible);
+	System.out.println("Release Year :"+ releaseYear);
+	System.out.println("Browser Logo :"+ browserLogo);
+}
+}
